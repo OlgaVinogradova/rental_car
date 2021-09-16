c164e9fcd2eb7df774142bde3c44f5a735b66e2c
OlgaVinogradova <49756613+OlgaVinogradova@users.noreply.github.com>
2021-09-16 13:27:01 +0300

    V03 change active class

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-import { PagesData } from '../helper/PagesData';
+import { PagesData } from '../Slider/PagesData';
 import Inst from '../svg/inst';
 import Facebook from '../svg/facebook';
 import Telegram from '../svg/telegram';
